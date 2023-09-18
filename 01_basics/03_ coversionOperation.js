0b190a21611a6aed2aed36c73f2f054ee3d92680
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-18 09:51:25 +0000

    conversion of datatype

---
+++ 01_basics/03_ coversionOperation.js
@@ -0,0 +1,25 @@
+// let score =false;
+// console.log(typeof(score));
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber)
+
+
+
+// "33" => 33
+// "33abc" => NaN 
+// true => 1;  false=> 0
+let = isLoggedIn= "123";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false 
+// "Amit" => true
+
+
+let someNumber = 95;
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber)
