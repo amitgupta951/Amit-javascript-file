4b33f361dfe727ea22484aae1e82c01b54cd2f44
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-18 12:22:02 +0000

    DataType-Summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,8 @@
+const score=100;
+const scoreValue =100.5;
+const isLoggedIn = false;
+const outsideTemp = null;
+let useremail;
+const id = Symbol(123);
+const anotherId =Symbol('123');
+console.log(id ===anotherId);
