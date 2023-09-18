769a3b6c3bf208142ba318f9e369a49be927b965
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-18 12:08:16 +0000

    change

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,13 +1,13 @@
-// console.log("2">1)
-//  console.log(null>0);
-//  console.log(null==0);
-//  console.log(null>=0);
-//  console.log(null<=0);
+console.log("2">1)
+ console.log(null>0);
+ console.log(null==0);
+ console.log(null>=0);
+ console.log(null<=0);
 
 
-//  console.log(undefined==0);
-//  console.log(undefined >0);
-//  console.log(undefined < 0);
+ console.log(undefined==0);
+ console.log(undefined >0);
+ console.log(undefined < 0);
 
  // ====
  console.log("2"===2);
