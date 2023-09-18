68c2ff5e0c7ba1b1a0d6ddf81787fa9df9197f94
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-18 10:37:37 +0000

    operation of datatype

--- 01_basics/03_ coversionOperation.js
+++ 01_basics/03_ coversionOperation.js
@@ -1,3 +1,5 @@
+          // Conversion  //
+
 // let score =false;
 // console.log(typeof(score));
 // let valueNumber = Number(score)
@@ -23,3 +25,36 @@ let someNumber = 95;
 let stringNumber = String(someNumber)
 console.log(someNumber)
 console.log(typeof stringNumber)
+
+
+                //  Operation  //
+let value =5;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Amit ";
+let str2 = "Gupta"
+let c = str1+ str2
+console.log(c);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+
+console.log(true)
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3;
+num1= num2 = num3 = 2+2;
+
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
