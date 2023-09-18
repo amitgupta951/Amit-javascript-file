0562691f1ceee15535abf3ed24578879c2270f60
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-18 08:53:52 +0000

    datatype

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accNo =3521586854;
+let accEmail = "amitgupta@gmail.com"
+var accPass = "12345"
+accCity = " Mumbai"
+// accNo = 454544454  // not allowed
+accEmail = "amitgupta82@gmail.com"
+accPass= "25445"
+accCity = "jaipur"
+
+let accState; 
+console.log(accNo)
+
+/*
+prefer not use var bcz of issue i block scope and function scope
+*/
+
+console.table([accNo,accEmail,accPass,accCity,accState]);
