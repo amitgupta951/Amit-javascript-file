f186fa86ab54ae7fa057a675aa4e5b6bf52adda1
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-18 08:55:12 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+"Use strict";  // treat all js as newer version
+// alert(3+3);  // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("hello")
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//bollean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof "rhfhr");
