a962cc4518234d0908fb6a7baee75e93fab57686
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-20 04:52:20 +0000

    datatype summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -6,3 +6,21 @@ let useremail;
 const id = Symbol(123);
 const anotherId =Symbol('123');
 console.log(id ===anotherId);
+
+const bigNumber = 123456678796345454n;
+
+
+// Refrence  (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Amit", "Ajay", "Ganesh"]
+let myObj={
+    name: "Amit",
+    age:  22
+}
+const myFunction =  function(){
+        console.log("Hello World")
+}
+
+console.log(typeof bigNumber)
