635e99a1431db92206dd7530a4d40d22c571563b
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-20 11:42:20 +0000

    array part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+// array
+const myArray = [5,1,2,3,4,5,6];
+// console.log(myArray[0])
+
+//      Array Methods
+
+// myArray.push(7)
+// myArray.push(9)
+// myArray.pop()
+// console.log(myArray)
+
+// myArray.unshift(10)
+// myArray.shift()
+
+// console.log(myArray.includes(15))
+
+// const newArr = myArray.join()
+// console.log(myArray)
+// console.log(newArr)
+
+//       Slice and Splice
+console.log("A", myArray);
+const myn1 = myArray.slice(1,3);
+
+
+console.log(myn1);
+console.log("B",myArray);
+
+const myn2 = myArray.splice(1,3);
+console.log("C",myArray);
+
+console.log(myn2);
