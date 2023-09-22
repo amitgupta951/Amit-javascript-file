d5c2d1fde5bc056467bf4b77901097efaf2ee9fa
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-22 07:26:25 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,45 @@
+//  var c = 300
+let a =500;
+ if(true){
+    let a =54;
+    const b = 45;
+    // console.log("INNER:", a);
+
+ }
+//  console.log(a)
+//  console.log(b)
+//  console.log(c)
+
+function one(){
+    const username ="Amit"
+    function Two(){
+        const website ="Youtube"
+        // console.log(username);
+    }
+    // console.log(website)
+
+    Two()
+}
+one()
+
+if(true){
+    const username ="Amit"
+    if(username==="Amit"){
+        const website=" Youtube"
+        // console.log(username+website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+addone(5)
+function addone(num){
+    return num+1
+}
+
+
+const addTwo =  function(num){
+    return num + 2
+}
+addTwo(4)
