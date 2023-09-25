71cd1675eabe67dcf04dd932daae9c52f9ded94d
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-25 06:18:48 +0000

    switch statement

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,21 @@
+const month = "March";
+ 
+switch(month){
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+        default:
+            console.log("default case match");
+            break;
+}
+
