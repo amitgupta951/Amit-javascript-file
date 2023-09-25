67afe9b18deb848504420fb2a226eb532a48145f
amitgupta951 <114480023+amitgupta951@users.noreply.github.com>
2023-09-25 08:49:52 +0000

    Loops

---
+++ 05_iterartions/02_loops.js
@@ -0,0 +1,23 @@
+// While Loops
+let i =0;
+while (i<=10) {
+    // console.log("The value of index is:" +i)
+    i=i+2;
+}
+
+let myArray = ["Hello", "Amit", "Hey","Namashte"]
+
+let arr = 0;
+while(arr<myArray.length){
+    // console.log(`value is ${myArray[arr]}`);
+    arr++
+}
+
+
+//  Do While Loop
+
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10)
